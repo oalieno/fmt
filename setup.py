d85b823dd3602634b80b1f801940aab8a4c02d1a
oalieno <jeffrey6910@gmail.com>
2019-08-08 18:06:58 -0700

    refactor

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name='fmt',
+    version='0.0.2',
+    description='automated format string',
+    install_requires=[],
+    author='oalieno',
+    author_email='jeffrey6910@gmail.com',
+    url='https://github.com/oalieno/fmt',
+    license='MIT License',
+    py_modules=[ 'fmt' ],
+)
